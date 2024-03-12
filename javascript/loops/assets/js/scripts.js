70c1f7fb9efc01494481e80e3a1ad9881517e880
aholmina <158294462+aholmina@users.noreply.github.com>
2024-03-12 22:18:36 +0800

    activity

--- javascript/loops/assets/js/scripts.js
+++ javascript/loops/assets/js/scripts.js
@@ -1,6 +1,7 @@
 let Multiply = document.getElementById("btnMultiply");
 
 Multiply.addEventListener('click', function() {
+
     let RowLimit = document.getElementById("RowLimit").value;
     let ColLimit = document.getElementById("ColLimit").value;
     let Products = "";
@@ -12,12 +13,13 @@ Multiply.addEventListener('click', function() {
         
         for(var col = 1; col <= ColLimit; col++){
             Products += "<td style='width: 30px;'>"+(row * col)+"</td>";
-            
-            
-    }
-    tableTD += `<tr class='border-bottom : 1px solid #fff;'>${Products}</tr>`;
+        }
+        tableTD += `<tr class='border-bottom : 1px solid #fff;'>${Products}</tr>`;
 
-    document.getElementById("div-wrapper").innerHTML = tableTD;
+        const divWrapper = document.getElementById("div-wrapper");
+        if (divWrapper) {
+            divWrapper.innerHTML = tableTD;
+        }
     }
 
     
